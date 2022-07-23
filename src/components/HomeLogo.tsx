a747016f6a9c462653dee49f0dd915b515369d05
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2022-07-23 14:54:03 +0000

    Home Link

--- src/components/HomeLogo.tsx
+++ src/components/HomeLogo.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 import styles from "../styles/components.module.css";
@@ -7,9 +8,11 @@ export default function HomeLogo(): JSX.Element {
 
     <div className={styles.logo}  >
 
+      <Link href={"/"} >
       <p>
         CRUD Todo App
       </p>
+      </Link>
     </div>
 
 

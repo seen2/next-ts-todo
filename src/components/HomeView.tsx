d864f1e72e82f079605eadeb5b46b49cd9faa88d
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2022-07-24 12:55:29 +0000

    Thanks Users

--- src/components/HomeView.tsx
+++ src/components/HomeView.tsx
@@ -8,15 +8,30 @@ import dynamic from 'next/dynamic';
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import { RootState } from '../redux/store';
 import ShowMessage from './ShowMessage';
-import { setLoading, setMsg, setStatusCode } from '../redux/reducers/authReducers';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Image, Text } from '@nextui-org/react';
 
 const DynamicClock = dynamic(() => import('./Clock'), {
   ssr: false,
 })
 
+
+
 const HomeView = () => {
+
+  const [users,setUsers]=useState([]);
+
+  useEffect(()=>{
+    const fetchUser=async ()=>{
+      const result=await fetch("/api/users");
+      const {names}=await result.json() ||{names:[]};
+      setUsers(names.sort());
+    }
+    
+    return ()=>{
+      fetchUser();
+    }
+  },[])
 
   const { statusCode, msg } = useAppSelector((state: RootState) => state.auth);
 
@@ -33,6 +48,17 @@ const HomeView = () => {
         <Image src='https://img.icons8.com/ios-glyphs/452/github.png' alt='source code' height={"50px"} width={"50px"} /> 
       </a>
       
+      </div>
+      <div 
+      style={{
+        display:"flex",justifyContent:"center",
+        color:"white",alignItems:"center",
+        flexWrap:"wrap",flexDirection:"column",
+        backgroundColor:"#1a1a1a",
+        padding:"0.7rem",
+        borderRadius:"0.7rem"
+        }}  >
+        {users.map((user,i)=><Text color='white' size={"30px"} key={i} >{i+1}. {user}, Thank You for Using.</Text>)}
       </div>
     </div>
 

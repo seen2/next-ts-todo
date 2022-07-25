949c0a2cf6833bc973d0721514bc5edc73268205
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2022-07-25 17:30:09 +0000

    Environment Variable Fix

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -14,9 +14,9 @@ export default function handler(
   res.status(200).json({ name: 'John Doe' });
 }
 
-export const environmentVariables={
-  uri:process.env.DB_URI,
-  jwtKey:process.env.JWT_KEY,
-  bcryptSaltRounds:process.env.BCRYPT_SALT_ROUNDS
+export const environmentVariables:{uri:string ,jwtKey:string,bcryptSaltRounds:number |string}={
+  uri:process.env.DB_URI ||"",
+  jwtKey:process.env.JWT_KEY || "",
+  bcryptSaltRounds:process.env.BCRYPT_SALT_ROUNDS || 0
 
-}
+};
--- src/util/appconfig.ts
+++ src/util/appconfig.ts
@@ -1,4 +1,7 @@
 import { environmentVariables } from "../pages/api/hello";
 
 
-export const {uri,jwtKey,bcryptSaltRounds}=environmentVariables;
+export const {uri ,jwtKey}=environmentVariables;
+
+export const bcryptSaltRounds=Number(environmentVariables.bcryptSaltRounds);
+ 
